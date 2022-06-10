88230e40651e33e94bc1e137836c6f2630688089
Ramesha <rameshagowda555@gmail.com>
2022-06-10 17:10:00 +0530

    demoproj

---
+++ demoproj/src/main/java/com/leanspring/demoproj/App.java
@@ -0,0 +1,13 @@
+package com.leanspring.demoproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
